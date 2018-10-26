e150025950e4dbe9d74ea691a3f2ec10c7b2b30d
Tom De Backer <tom.debacker@realdolmen.com>
2018-10-26 15:28:59 +0200

    test

---
+++ src/main/java/App.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args){
+        System.out.println("What would you like to do?\n\n1. Add\n2. Multiply\n3. Divide");
+    }
+}
